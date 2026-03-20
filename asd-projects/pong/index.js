558fb8cc8120ec99a79ce5738bea9920a1d4ee29
alexislombardo <amlombardo4@gmail.com>
2026-03-20 17:10:42 +0000

    saved pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -10,6 +10,8 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+  const BOARD_WIDTH = $("#board").width();
+  const BOARD_HEIGHT = $("#board").height();
   const KEY = {
     UP: 38,
     DOWN: 40,
@@ -18,24 +20,22 @@ function runProgram(){
   };
   
   // Game Item Objects
-  function createGameItem(id, x, y, speedX, speedY, width, height){
+  function createGameItem(id, x, y, speedX, speedY){
     return {
       id: id,
       x: x,
       y: y,
       speedX: speedX,
       speedY: speedY,
-      width: width,
-      height: height
+      width: $(id).width(),
+      height: $(id).width()
     }
   }
 
-  
-  
-  var pingBall = createGameItem("gameItem", 0, 0, 0, 0, 20, 20);
+  var pingBall = createGameItem("#pingPong", 0, 0, 0, 0);
   startBall(); // sets the position and gives the ball a random speed
-  var leftPaddle = createGameItem("leftPaddle", 0, 260, 0, 0, 20, 80);
-  var rightPaddle = createGameItem("rightPaddle", 480, 260, 0, 0, 20, 80);
+  var leftPaddle = createGameItem("#leftPaddle", 0, 260, 0, 0);
+  var rightPaddle = createGameItem("#rightPaddle", BOARD_WIDTH - $("#rightPaddle").width(),  260, 0, 0);
   
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
@@ -52,7 +52,7 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-     pingBall.x += pingBall.speedX;
+    pingBall.x += pingBall.speedX;
     pingBall.y += pingBall.speedY;
 
   // bounce off left/right walls
@@ -64,14 +64,17 @@ function runProgram(){
     if (pingBall.y <= 0 || pingBall.y + pingBall.height >= 600) {
       pingBall.speedY *= -1;
     } 
-    // handles pandle movement
-    movePaddles();
-    keepPaddlesInBounds();
-
     // draws the game items 
     drawGameItem(pingBall);
     drawGameItem(leftPaddle);
     drawGameItem(rightPaddle);
+
+    // handles pandle movement
+    movePaddles();
+    keepPaddlesInBounds(leftPaddle);
+    keepPaddlesInBounds(rightPaddle);
+
+  
 
   }
   
@@ -139,25 +142,19 @@ function runProgram(){
     rightPaddle.y += rightPaddle.speedY;
   }
 
-  function keepPaddlesInBounds() {
+  function keepPaddlesInBounds(paddle) {
     // left paddle
-    if (leftPaddle.y < 0) leftPaddle.y = 0;
-    if (leftPaddle.y + leftPaddle.height > 600) {
-      leftPaddle.y = 600 - leftPaddle.height;
+    if (paddle.y < 0) paddle.y = 0;
+    if (paddle.y > BOARD_HEIGHT - paddle.height) {
+      paddle.y = BOARD_HEIGHT - paddle.height;
     }
 
-    // right paddle
-    if (rightPaddle.y < 0) rightPaddle.y = 0;
-    if (rightPaddle.y + rightPaddle.height > 600) {
-      rightPaddle.y = 600 - rightPaddle.height;
-    }
 }
 
 
     function drawGameItem(item) {
-      var element = document.getElementById(item.id);
-      element.style.left = item.x + "px";
-      element.style.top = item.y + "px";
+    $(item.id).css("top", item.y);
+    $(item.id).css("left", item.x);
 }
   
   function endGame() {
